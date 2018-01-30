9c09712ca1e698072c7bed4c7ab07790d2e98f41
Andresito de Guzman <deguzman.andresito@gmail.com>
2018-01-30 13:25:07 +0800

    Update tests.php

--- tests.php
+++ tests.php
@@ -6,6 +6,11 @@ Ensures that everything works
 
 require_once __DIR__."/dist/kunin.php";
 
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 use PHPUnit\Framework\TestCase;
 
 class test extends TestCase{
